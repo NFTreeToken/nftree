17c118d2d01bf42d1e4307b00fb3505777f9257f
Theo Ephraim <theozero@gmail.com>
2021-03-21 04:08:23 -0700

    stuff

--- packages/nftree-frontend/src/utils/covalent.ts
+++ packages/nftree-frontend/src/utils/covalent.ts
@@ -1,4 +1,5 @@
 import Axios from 'axios';
+import { useEffect, useState } from 'react';
 
 const API_KEY = 'ckey_aa9570ccd4e04f91b8928065f92';
 
@@ -28,4 +29,44 @@ export async function getTokenPriceData(
     params: { from: startDateISO, to: endDateISO },
   });
   return result.data.prices;
+}
+
+export function usePriceData(tokenSymbol, startDateISO, endDateISO) {
+  const [isLoading, setIsLoading] = useState(true);
+  const [prices, setPrices] = useState(false);
+
+  useEffect(() => {
+    if (!startDateISO || !endDateISO) return;
+    (async () => {
+      const pricesResult = await getTokenPriceData(tokenSymbol, startDateISO, endDateISO);
+      setPrices(pricesResult);
+      setIsLoading(false);
+    })();
+  }, [tokenSymbol, startDateISO, endDateISO]);
+
+  return [prices, isLoading];
+}
+
+export async function getBlockDate(
+  blockNumber: string,
+) {
+  const result = await covalentApi({
+    method: 'get',
+    url: `1/block_v2/${blockNumber}/`,
+  });
+  return result.data.data.items[0].signed_at.substr(0, 10);
+}
+export function useBlockDate(blockNumber) {
+  const [isLoading, setIsLoading] = useState(true);
+  const [blockDate, setBlockDate] = useState();
+
+  useEffect(() => {
+    (async () => {
+      const blockDateResult = await getBlockDate(blockNumber);
+      setBlockDate(blockDateResult);
+      setIsLoading(false);
+    })();
+  }, [blockNumber]);
+
+  return [blockDate, isLoading];
 }
